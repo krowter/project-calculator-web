75def1761cc83804690b775c73966bd505037bf5
Krishna Rowter <krishna.rowter@gmail.com>
2019-07-02 21:11:20 +0700

    fix button functionality

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
+let counterForPeriod = 0;
+let counterForOperators = 0;
+
+let checkStringLength = () => {
+    let flag = (document.getElementById("result-text").textContent.length > 10)? true:false;
+    
+    let allButton = document.querySelectorAll("button");
+    for(let i=0;i<allButton.length;i++) {
+        allButton[i].disabled=flag;
+    }
+    document.getElementById("backspace").disabled=false;
+    
+}
+
+let updatePeriodButton = () => {
+    document.getElementById("period").disabled = counterForPeriod>0 ? true : false;
+}
+
 let sendEvent = (sender) => {
+    checkStringLength();
     document.getElementById("result-text").innerHTML += sender.textContent;
+    if(sender.textContent==".") counterForPeriod++;
     console.log(sender.innerHTML);
+    enableOperators();
+    if(sender.className=="operand") {
+        counterForOperators=0;
+    }
+    if(sender.className=="operator") {
+        counterForOperators++;
+        sender.disabled=true;
+    }
+    if(sender.className=="operator" && counterForOperators>1) {
+        backspace();
+        backspace();
+        document.getElementById("result-text").innerHTML += sender.textContent;
+    }
+    updatePeriodButton();
 }
 
 let evaluateResult = () => {    
@@ -10,11 +44,28 @@ let evaluateResult = () => {
 }
 
 let backspace = () => {
+    checkStringLength();
     let resultString = String(document.getElementById("result-text").innerHTML);
+    if(resultString.charAt(resultString.length-1).toString()=="*") enableOperators();
     resultString = resultString.slice(0,-1);
     document.getElementById("result-text").innerHTML = resultString;
 }
 
 let clearTextbox = () => {
     document.getElementById("result-text").innerHTML = "";
+    enableOperators();
+}
+
+let operator = document.getElementsByClassName("operator");
+
+let enablePeriod = () => {
+    counterForPeriod = 0;
+    updatePeriodButton();
+}
+
+let enableOperators = () => {
+    operators = document.getElementsByClassName("operator");
+    for(let i=0;i<operators.length;i++) {
+        operators[i].disabled=false;
+    }
 }
